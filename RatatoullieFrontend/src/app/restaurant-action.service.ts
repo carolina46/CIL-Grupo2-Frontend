28f76df7a2b7342cd50c7cfaf5c70b5f7a20639e
Federico Pantaleone <fragsman@gmail.com>
2018-08-14 14:10:23 -0300

    added comments to the services to know what will they do

--- RatatoullieFrontend/src/app/restaurant-action.service.ts
+++ RatatoullieFrontend/src/app/restaurant-action.service.ts
@@ -1,3 +1,4 @@
+// This service will be responsible for providing action operations such as Vote, Recommend on Restaurant, Menu, Dish and related classes
 import { Injectable } from '@angular/core';
 
 @Injectable({
--- RatatoullieFrontend/src/app/restaurant-administration.service.ts
+++ RatatoullieFrontend/src/app/restaurant-administration.service.ts
@@ -1,3 +1,4 @@
+// This service will be responsible for providing operations such as Create, Delete, Modify for Restaurant, Menu, Dish and related classes
 import { Injectable } from '@angular/core';
 
 @Injectable({
