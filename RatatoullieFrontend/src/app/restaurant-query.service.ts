d2f940ac62b551f9c1e5eba363b82d562ae94c0b
Federico Pantaleone <fragsman@gmail.com>
2018-08-14 22:05:51 -0300

    removed unnecessary imports

--- RatatoullieFrontend/src/app/restaurant-query.service.ts
+++ RatatoullieFrontend/src/app/restaurant-query.service.ts
@@ -3,16 +3,20 @@ list and search for Restaurant, Menu, Dish and related classes.*/
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Category } from './model/business/category';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
+import { ParentServiceService } from './parent-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RestaurantQueryService {
+export class RestaurantQueryService extends ParentServiceService {
 
-  constructor(private http: HttpClient, private messageService: MessageService) { }
+  constructor(private http: HttpClient,
+    messageService: MessageService) {
+    super(messageService);
+  }
 
   private categoriesGetURL = 'http://localhost:8080/Ratatoullie/category/listCategory';
   private categoryURL = ''; // TO BE IMPLEMENTED ON THE SERVER!
@@ -31,36 +35,12 @@ export class RestaurantQueryService {
   getCategory(id: number): Observable<Category> {
     const url = `${this.categoryURL}/${id}`;
     return this.http.get<Category>(url).pipe(
-        tap(_ => this.log(`retrieved category with id=${id}`)),
-        catchError(this.handleError<Category>(`getCategory id=${id}`))
+        tap(_ => super.log(`retrieved category with id=${id}`)),
+        catchError(super.handleError<Category>(`getCategory id=${id}`))
       );
   }
 
   getMenuTypes() {
     return this.http.get('http://localhost:8080/Ratatoullie/menuType/listMenuType');
-  }
-
-  // Send the message log to the Message Service
-  private log(message: string) {
-    this.messageService.add(`RestaurantQueryService: ${message}`); // Note the special quotation marks
-  }
-
-  /**
- * Handle Http operation that failed. Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-  private handleError<T> (operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
   }
 }
