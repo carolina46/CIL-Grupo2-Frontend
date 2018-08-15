b86d5225c72d651f1fdcce81e9af1b74df397caf
Federico Pantaleone <fragsman@gmail.com>
2018-08-14 21:35:57 -0300

    modify to inherit from ParentService

--- RatatoullieFrontend/src/app/restaurant-administration.service.ts
+++ RatatoullieFrontend/src/app/restaurant-administration.service.ts
@@ -1,10 +1,39 @@
 // This service will be responsible for providing operations such as Create, Delete, Modify for Restaurant, Menu, Dish and related classes
 import { Injectable } from '@angular/core';
+import { Category } from './model/business/category';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { ParentServiceService } from './parent-service.service';
+import { MessageService } from './message.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class RestaurantAdministrationService {
+export class RestaurantAdministrationService extends ParentServiceService{
 
-  constructor() { }
+  private categoryPostURL = 'http://localhost:8080/Ratatoullie/category/categoryForm';
+
+  constructor(private http: HttpClient,
+    private route: ActivatedRoute,
+    private location: Location,
+    messageService: MessageService) {
+      super(messageService);
+   }
+
+  /** POST: add a new category to the server */
+  addCategory (category: Category): Observable <Category> {
+    return this.http.post<Category>(this.categoryPostURL, category, httpOptions).pipe(
+      tap((category: Category) => super.log(`added category w/ id=${category.id}`)),
+      catchError(super.handleError<Category>('addCategory'))
+    );
+  }
+
+
 }
