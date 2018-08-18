37b86e9166b7ea4b6fde1fb3a5b4642996f1039b
carolina <mariacarola46@hotmail.com>
2018-08-18 00:37:38 -0300

    getTags in menuForm

--- RatatoullieFrontend/src/app/menu-form/menu-form.component.ts
+++ RatatoullieFrontend/src/app/menu-form/menu-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RestaurantQueryService } from '../restaurant-query.service';
 import { Menu } from '../model/business/menu';
 import { MenuType } from '../model/business/menu-type';
+import { Tag } from '../model/business/tag';
 import {Router} from "@angular/router";
 
 
@@ -14,10 +15,11 @@ export class MenuFormComponent implements OnInit {
 
   menu = new Menu();
   menuType = new MenuType();
-  id;
+  
 
   menuTypes: MenuType[];
-  menuTypeSelect;
+  tags: Tag[];
+  
 
   
   // TODO: Remove this when we're done
@@ -31,11 +33,17 @@ export class MenuFormComponent implements OnInit {
 
   ngOnInit() {
     this.getMenuTypes();
+    this.getTags();
   }
 
   getMenuTypes(): void {
     this.restaurantQueryService.getMenuTypes().subscribe(menuTypes => this.menuTypes = menuTypes);
   }
+
+  getTags():void{
+    this.restaurantQueryService.getTags().subscribe(tags => this.tags = tags);
+  }
+
 
 
 }
