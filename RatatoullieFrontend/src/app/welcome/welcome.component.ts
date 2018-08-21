83fb6edb8535a9fcb8434cbf413a2d62c396254b
carolina <mariacarola46@hotmail.com>
2018-08-21 01:46:15 -0300

    add some methods to welcomeComponent

--- RatatoullieFrontend/src/app/welcome/welcome.component.ts
+++ RatatoullieFrontend/src/app/welcome/welcome.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Login } from '../model/users/login';
 import { Signin } from '../model/users/signin';
+import { LocalStorageServiceService } from '../local-storage-service.service';
+import {Router} from '@angular/router';
+import { MenuComponent } from 'src/app/menu/menu.component';
 
 @Component({
   selector: 'app-welcome',
@@ -9,39 +12,57 @@ import { Signin } from '../model/users/signin';
 })
 export class WelcomeComponent implements OnInit {
 
-  login : Login; //To save log in information
-  signIn : Signin; //To save sign in information
+  login : Login; //To save login information
+  signIn : Signin; //To save signin information
   showSignInForm : boolean; //To control if show introduction text or sign in form
   responsible : boolean; //To know what type of user is gonna be
   
-  constructor() {}
+  constructor(private localStorage: LocalStorageServiceService,
+              private router: Router) { }
 
   ngOnInit() {
     this.showSignInForm = false;//Show introduction text
     this.login = new Login();
-    this.login.user="";
-    this.login.password="";
+    this.clearLogInProperty;
     this.signIn = new Signin();
+    this.clearSignInProperty();
   }
 
-  logIn(){ 
-    if(this.login.password.length > 0 && this.login.user.length > 0 )
-      console.log("Comprobar usuario");
-      
-
+  logIn(){ //Method to enter the system
+    //if(this.login.password.length > 0 && this.login.user.length > 0 )
+    //  console.log("Comprobar usuario");
+    this.localStorage.storeUserOnLocalStorage("aUser");
+    this.router.navigate(['/principal']);
+    location.reload();
    }
 
   showFormSignIn(){
-    console.log("registrarse");
     this.showSignInForm = true;
   }
 
-  singIn(){ 
-
+  singIn(){ //Methodo to add a new user 
+    this.showSignInForm = false;
+    this.clearSignInProperty();
+    //Comprobar que la propiedad user no exista
+    //llevar datos servidor
   }
    
-  cancelSignIn(){ }
+  cancelSignIn(){ 
+    this.showSignInForm = false;
+    this.clearSignInProperty();
+  }
 
+  clearSignInProperty(){
+    this.signIn.fullName = "";
+    this.signIn.password = "";
+    this.signIn.user = "";
+    this.responsible = null;
+  }
+
+  clearLogInProperty(){
+    this.login.user="";
+    this.login.password="";
+  }
   
 
 }
