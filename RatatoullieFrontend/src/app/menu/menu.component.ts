cebd924d9db3fb97807fc2e2ecfc4a27408ced4e
carolina <mariacarola46@hotmail.com>
2018-08-21 01:44:38 -0300

    modify menuComponent

--- RatatoullieFrontend/src/app/menu/menu.component.ts
+++ RatatoullieFrontend/src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageServiceService } from '../local-storage-service.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private localStorage: LocalStorageServiceService,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
+  userLogout(){
+    this.localStorage.removeUserFromLocalStorage();
+    this.router.navigate(['/']);
+    location.reload();
+  }
 }
