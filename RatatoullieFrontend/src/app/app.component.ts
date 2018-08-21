fafb50dfd14ea7a9510ed8ea08aec666a7047767
carolina <mariacarola46@hotmail.com>
2018-08-21 01:48:06 -0300

    I control if the user start session

--- RatatoullieFrontend/src/app/app.component.ts
+++ RatatoullieFrontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { LocalStorageServiceService } from './local-storage-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'RatatoullieFrontend';
+  userIsLogin : Boolean;
+
+  constructor(private localStorage: LocalStorageServiceService) { }
+
+  ngOnInit(){
+    if (this.localStorage.getUserFromLocalStorage() != null) this.userIsLogin = true;
+    else this.userIsLogin = false;
+  }
+
+  
 }
