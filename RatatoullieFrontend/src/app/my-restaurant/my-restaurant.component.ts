674286a827b163c93c97430027faa4a1bbcbd02f
carolina <mariacarola46@hotmail.com>
2018-08-23 21:52:11 -0300

    Add content to myRestaurantComponent

--- RatatoullieFrontend/src/app/my-restaurant/my-restaurant.component.ts
+++ RatatoullieFrontend/src/app/my-restaurant/my-restaurant.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Menu } from '../model/business/menu';
 
 @Component({
   selector: 'app-my-restaurant',
@@ -10,12 +11,33 @@ export class MyRestaurantComponent implements OnInit {
 
   constructor(private router : ActivatedRoute) { }
 
-  id: number;
+  id : number;
+  menus : Menu[];
 
   ngOnInit() {
     this.router.params.subscribe( params => {
       this.id = params['id'].toString();
   });
+
+  //Debemos buscar en la bd el reto con sus menus.
+  
+  this.menus=[];
+  let m : Menu;
+  m = new Menu();
+  m.name="Menu1"
+  this.menus.push(m);
+  m = new Menu();
+  m.name="Menu2"
+  this.menus.push(m);
+  m = new Menu();
+  m.name="Menu3"
+  this.menus.push(m);
+  m = new Menu();
+  m.name="Menu4"
+  this.menus.push(m);
+  
+
+
   }
 
 }
