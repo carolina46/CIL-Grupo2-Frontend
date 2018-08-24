9a0c3e302795d15e74af782c9b36678b49a85662
Federico Pantaleone <fragsman@gmail.com>
2018-08-24 20:25:36 -0300

    modify message service to limit an amount of visible messages

--- RatatoullieFrontend/src/app/message.service.ts
+++ RatatoullieFrontend/src/app/message.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Component } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
   messages: string[] = [];
+  maxSize: number = 3; // Max size for the messages, then it will delte the oldest
 
   constructor() { }
 
   add(message: string) {
+    if(this.messages.length == this.maxSize) // I must delete the oldest message
+      this.messages.splice(0,1); // From the position 0 removes 1 element
     this.messages.push(message);
   }
   clear() {
