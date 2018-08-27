5cc517d2d7f0b3311df18ed6f808304d63a6660f
carolina <mariacarola46@hotmail.com>
2018-08-27 17:05:32 -0300

    Add the path to add a dish

--- RatatoullieFrontend/src/app/app-routing.module.ts
+++ RatatoullieFrontend/src/app/app-routing.module.ts
@@ -7,20 +7,23 @@ import { MenuFormComponent } from './form-menu/form-menu.component';
 import { DishFormComponent } from './form-dish/form-dish.component';
 import { MyRestaurantsComponent } from './my-restaurants/my-restaurants.component';
 import { MyRestaurantComponent } from './my-restaurant/my-restaurant.component';
+import { MyMenuComponent } from './my-menu/my-menu.component';
 import { SaveMenuTypeComponent } from './form-menu-type/form-menu-type.component';
 import { FormRestaurantComponent } from './form-restaurant/form-restaurant.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/principal', pathMatch: 'full' },
   { path: 'listCategories', component: ListCategoryComponent },
   { path: 'principal', component: PrincipalComponent },
-  { path: 'dishForm', component: DishFormComponent },
   { path: 'saveMenuType', component: SaveMenuTypeComponent },
   { path: 'myRestaurants', component: MyRestaurantsComponent },
   { path: 'myRestaurants/restaurant/:id', component: MyRestaurantComponent },
   { path: 'myRestaurants/restaurant/:id/addMenu', component: MenuFormComponent },
+  { path: 'myRestaurants/restaurant/:idR/menu/:idM', component: MyMenuComponent },
+  { path: 'myRestaurants/restaurant/:idR/menu/:idM/addDish', component: DishFormComponent },
   { path: 'myRestaurants/addRestaurant', component: FormRestaurantComponent },
-  {  path: '**', redirectTo: '/principal'} //It should always be at the end of the list
+  { path: '**', redirectTo: '/principal'} //It should always be at the end of the list
 
 ];
 
