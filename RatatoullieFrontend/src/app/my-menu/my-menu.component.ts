a89b968cd22fd2e141b243b214143e53a2783c61
carolina <mariacarola46@hotmail.com>
2018-08-27 17:04:29 -0300

    Add some content to MyMenuComponent

--- RatatoullieFrontend/src/app/my-menu/my-menu.component.ts
+++ RatatoullieFrontend/src/app/my-menu/my-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-my-menu',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyMenuComponent implements OnInit {
 
-  constructor() { }
+  idRestaurant : number;
+  idMenu : number;
+
+  constructor(private router : ActivatedRoute) { }
 
   ngOnInit() {
+    this.router.params.subscribe( params => {
+    this.idRestaurant = params['idR'].toString();
+    this.idMenu = params['idM'].toString();
+});
   }
+
+  goBack(){
+    window.history.back();
+  }
+
 
 }
