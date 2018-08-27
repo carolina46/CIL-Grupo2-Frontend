852240a3057f6aff4359b5c3545674374782ba2f
Federico Pantaleone <fragsman@gmail.com>
2018-08-27 16:34:06 -0300

    added the comments and its import

--- RatatoullieFrontend/src/app/model/business/restaurant.ts
+++ RatatoullieFrontend/src/app/model/business/restaurant.ts
@@ -2,13 +2,15 @@ import { Category } from "./category";
 import { Menu } from "./menu";
 import { Location } from './location';
 import { CommentFilter } from "../filter/comment_filter";
-import { NotificationFilter } from "../filter/notification_filter";
+import { NotificationFilter } from '../filter/notification_filter';
+import { Comment } from '..//comment';
 
 export class Restaurant {
     oid: number;
     name: string;
     category: Category;
     menus: Menu[];
+    comments: Comment[];
     location: Location;
     commentFilter: CommentFilter;
     notificationFilter: NotificationFilter;
