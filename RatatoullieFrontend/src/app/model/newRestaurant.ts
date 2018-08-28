55bd25b9bd7bd89041c3dab6330b348f9763e553
carolina <mariacarola46@hotmail.com>
2018-08-28 20:11:48 -0300

    Create teh NewRestaurant class

---
+++ RatatoullieFrontend/src/app/model/newRestaurant.ts
@@ -0,0 +1,11 @@
+export class NewRestaurant {
+    responsible : number;
+    name : string;
+    category : number;
+    latitude: number;
+    longitude: number;
+    //Filtros
+    visitor : boolean;
+    comensal : boolean;
+    gourmet : boolean;
+}
