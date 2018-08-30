d2a49b6f22baeb80f65f6eec1a9048d85c8f2bf6
carolina <mariacarola46@hotmail.com>
2018-08-30 12:46:22 -0300

    add some validation

--- RatatoullieFrontend/src/app/my-menu/my-menu.component.ts
+++ RatatoullieFrontend/src/app/my-menu/my-menu.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router} from '@angular/router';
+import { RestaurantQueryService } from '../restaurant-query.service';
+import { UserSession } from '../model/users/user-session';
+import { LocalStorageServiceService } from '../local-storage-service.service';
+import { Restaurant } from '../model/business/restaurant';
+import { Menu } from '../model/business/menu';
 
 @Component({
   selector: 'app-my-menu',
@@ -8,16 +13,44 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MyMenuComponent implements OnInit {
 
+  /* The logged user */
+  userSession : UserSession; 
+  /* The id of the restaurant. We get it from the url */
   idRestaurant : number;
+  /* The id of the menu that we are going to show. We get it from the url */
   idMenu : number;
+  /* The restaurant and its menus. We get it from the bd */
+  restaurant : Restaurant;
+  menu : Menu;
 
-  constructor(private router : ActivatedRoute) { }
+  constructor(private router : Router,
+              private activatedRoute : ActivatedRoute,
+              private localStorage : LocalStorageServiceService,
+              private restaurantQueryService: RestaurantQueryService) { }
 
   ngOnInit() {
-    this.router.params.subscribe( params => {
-    this.idRestaurant = params['idR'].toString();
-    this.idMenu = params['idM'].toString();
-});
+      /* We check if a user is logged in */ 
+      this.userSession = this.localStorage.getUserFromLocalStorage();
+      if (this.userSession == null)  this.router.navigate(['/principal']);
+      else
+        /* We check if the user has a responsible role */
+        if(this.userSession.role != "Responsible") this.router.navigate(['/principal']);
+        else{
+            this.activatedRoute.params.subscribe( params => {
+                this.idRestaurant = params['idR'].toString();
+                this.idMenu = params['idM'].toString();
+                this.restaurantQueryService.getRestaurant(this.idRestaurant).subscribe(restaurant=> {
+                    if(restaurant == null)  this.router.navigate(['/principal']);
+                    else{
+                      this.restaurant = restaurant;
+                      //Look for the menu with oid = idMenu
+                      let menus = restaurant.menus.filter(m => m.oid !== this.idMenu);
+                      if(menus.length == 0) this.router.navigate(['/principal']);
+                      else  this.menu = menus[0];
+                    }
+                });  
+            });
+        }
   }
 
   goBack(){
